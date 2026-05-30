a0c30484b7a15dab02dc8ac9b12260597a3dd3b8
AnkitKumarHub <akkimessi710@gmail.com>
2026-05-30 14:43:05 +0530

    replace font with Poppins

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Hellow world</div>
+    <div>
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <p></p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus praesentium autem aut officia obcaecati soluta. Ipsa nobis eveniet veniam eum.
+    </div>
   );
 }
